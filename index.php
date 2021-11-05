985d7f8be0bc17eda57199e76e8f29a41c7226bc
SAwarm <jonasdasilvaelias@gmail.com>
2021-11-04 22:37:09 -0300

    comments

--- index.php
+++ index.php
@@ -6,12 +6,12 @@ use \App\Pix\Payload;
 use Mpdf\QrCode\QrCode;
 use Mpdf\QrCode\Output;
 
-$obPayload = (new Payload)->setPixKey('123456789000')
-                          ->setDescription('payment')
-                          ->setMerchantName('Test Name')
-                          ->setMerchantCity("Ararangua")
-                          ->setAmount(5.00)
-                          ->setTxid('TestId');
+$obPayload = (new Payload)->setPixKey('123456789000') // key pix
+                          ->setDescription('payment') // description of the pix
+                          ->setMerchantName('Test Name') // name people
+                          ->setMerchantCity("Ararangua") // city 
+                          ->setAmount(5.00) // price of pix
+                          ->setTxid('TestId'); // id pix
 
 $payloadQrCode = $obPayload->getPayload();
 
