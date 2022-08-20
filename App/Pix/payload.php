f69d0a3e6ecda299b9760d2217a7bdca345b26b8
jonas-elias <jonasdasilvaelias@gmail.com>
2022-08-19 22:33:54 -0300

    fix: refactor rules

--- App/Pix/payload.php
+++ App/Pix/payload.php
@@ -2,11 +2,14 @@
 
 namespace App\Pix;
 
-class Payload{
-        /**
-     * IDs do Payload do Pix
-     * @var string
-     */
+/**
+ * Class Payload
+ * 
+ * @package App\Pix
+ * @author <jonas-elias/>
+ */
+class Payload
+{
     const ID_PAYLOAD_FORMAT_INDICATOR = '00';
     const ID_MERCHANT_ACCOUNT_INFORMATION = '26';
     const ID_MERCHANT_ACCOUNT_INFORMATION_GUI = '00';
@@ -34,60 +37,65 @@ class Payload{
 
     private $amount;
 
-    public function setPixKey($pixKey){
+    public function setPixKey($pixKey)
+    {
         $this->pixKey = $pixKey;
         return $this;
     }
 
-    public function setDescription($description){
+    public function setDescription($description)
+    {
         $this->description = $description;
         return $this;
     }
 
-    public function setMerchantName($merchantName){
+    public function setMerchantName($merchantName)
+    {
         $this->merchantName = $merchantName;
         return $this;
     }
 
-    public function setMerchantCity($merchantCity){
+    public function setMerchantCity($merchantCity)
+    {
         $this->merchantCity = $merchantCity;
         return $this;
     }
 
-    public function setTxid($txid){
+    public function setTxid($txid)
+    {
         $this->txid = $txid;
         return $this;
     }
 
-    public function setAmount($amount){
+    public function setAmount($amount)
+    {
         $this->amount = (string) number_format($amount, 2, '.', '');
         return $this;
     }
 
-    private function getValue($id, $value){
+    private function getValue($id, $value)
+    {
         $size = str_pad(strlen($value), 2, '0', STR_PAD_LEFT);
-        return $id.$size.$value;
+        return $id . $size . $value;
     }
-    
-    private function getMerchantAccountInformation(){
-        $gui = $this->getValue(self::ID_MERCHANT_ACCOUNT_INFORMATION_GUI, 'br.gov.bcb.pix');
 
+    private function getMerchantAccountInformation()
+    {
+        $gui = $this->getValue(self::ID_MERCHANT_ACCOUNT_INFORMATION_GUI, 'br.gov.bcb.pix');
         $key = $this->getValue(self::ID_MERCHANT_ACCOUNT_INFORMATION_KEY, $this->pixKey);
 
-        $description = $this->getValue(self::ID_MERCHANT_ACCOUNT_INFORMATION_DESCRIPTION, $this->description);
-
-        return $this->getValue(self::ID_MERCHANT_ACCOUNT_INFORMATION, $gui.$key.$description);
+        return $this->getValue(self::ID_MERCHANT_ACCOUNT_INFORMATION, $gui . $key . $this->getValue(self::ID_MERCHANT_ACCOUNT_INFORMATION_DESCRIPTION, $this->description));
     }
 
-    private function getAdditionalDataFieldTemplate(){
-        $txid = $this->getValue(self::ID_ADDITIONAL_DATA_FIELD_TEMPLATE_TXID, $this->txid);
-
-        return $this->getValue(self::ID_ADDITIONAL_DATA_FIELD_TEMPLATE, $txid);
+    private function getAdditionalDataFieldTemplate()
+    {
+        return $this->getValue(self::ID_ADDITIONAL_DATA_FIELD_TEMPLATE,  $this->getValue(self::ID_ADDITIONAL_DATA_FIELD_TEMPLATE_TXID, $this->txid));
     }
 
-    private function getCRC16($payload) {
-        
-        $payload .= self::ID_CRC16.'04';        
+    private function getCRC16($payload)
+    {
+
+        $payload .= self::ID_CRC16 . '04';
         $polinomio = 0x1021;
         $resultado = 0xFFFF;
 
@@ -95,28 +103,30 @@ class Payload{
             for ($offset = 0; $offset < $length; $offset++) {
                 $resultado ^= (ord($payload[$offset]) << 8);
                 for ($bitwise = 0; $bitwise < 8; $bitwise++) {
-                    if (($resultado <<= 1) & 0x10000) $resultado ^= $polinomio;
+                    if (($resultado <<= 1) & 0x10000) {
+                        $resultado ^= $polinomio;
+                    }
+
                     $resultado &= 0xFFFF;
                 }
             }
         }
 
-        return self::ID_CRC16.'04'.strtoupper(dechex($resultado));
+        return self::ID_CRC16 . '04' . strtoupper(dechex($resultado));
     }
 
+    public function getPayload()
+    {
+        $payload = $this->getValue(self::ID_PAYLOAD_FORMAT_INDICATOR, '01') .
+            $this->getMerchantAccountInformation() .
+            $this->getValue(self::ID_MERCHANT_CATEGORY_CODE, '0000') .
+            $this->getValue(self::ID_TRANSACTION_CURRENCY, '986') .
+            $this->getValue(self::ID_TRANSACTION_AMOUNT, $this->amount) .
+            $this->getValue(self::ID_COUNTRY_CODE, 'BR') .
+            $this->getValue(self::ID_MERCHANT_NAME, $this->merchantName) .
+            $this->getValue(self::ID_MERCHANT_CITY, $this->merchantCity) .
+            $this->getAdditionalDataFieldTemplate();
 
-    public function getPayload(){
-        $payload = $this->getValue(self::ID_PAYLOAD_FORMAT_INDICATOR, '01').
-                   $this->getMerchantAccountInformation().
-                   $this->getValue(self::ID_MERCHANT_CATEGORY_CODE, '0000').
-                   $this->getValue(self::ID_TRANSACTION_CURRENCY, '986').
-                   $this->getValue(self::ID_TRANSACTION_AMOUNT, $this->amount).
-                   $this->getValue(self::ID_COUNTRY_CODE, 'BR').
-                   $this->getValue(self::ID_MERCHANT_NAME, $this->merchantName).
-                   $this->getValue(self::ID_MERCHANT_CITY, $this->merchantCity).
-                   $this->getAdditionalDataFieldTemplate();
-
-        return $payload.$this->getCRC16($payload);
+        return $payload . $this->getCRC16($payload);
     }
 }
-
